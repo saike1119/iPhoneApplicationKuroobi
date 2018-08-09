1170e34c6e3fd2bd1f6dfd96fbbb51d6c98b883d
saike1119 <saike1119@gmail.com>
2018-08-09 21:46:26 +0900

    first commit

---
+++ Chapter2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,67 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+1+2
+10-1
+10*2
+10/3
+10%3
+
+"こんにちは"
+
+
+// var 変数
+var hw = "Hello" + "World"
+print(hw)
+
+var value: String = "Hello!"
+var num = 1
+num = 2
+num += 3
+
+//let 定数
+let fruit = "mikan"
+//fruit = "banana"
+
+var a = 1
+let b = 2
+
+var fruits = "りんごが\(a),みかんが\(b)"
+
+//数
+var num1 = Int.max
+var num2 = Int64.max
+
+//bool
+var switchOn = true
+var switchOff = false
+
+//浮動小数点
+//float 32bit
+var f = 12.34
+//double 64bit
+var d = 123.45
+
+//キャスト
+let cas1 = 3
+let cas2 = 0.14159
+
+let casNum = Double(cas1) + cas2
+
+//配列
+//可変配列
+var array1: [String] = [String]()
+array1.append("りんご")
+array1.append("みかん")
+array1.append("ばなな")
+
+//不変配列
+let array2: [String] = ["りんご","ごりら","らっぱ"]
+
+
+
+
+
