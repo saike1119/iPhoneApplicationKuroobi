ecb2cbf93674bce1af8d85c84995af322075978d
saike1119 <saike1119@gmail.com>
2018-08-09 23:25:34 +0900

    基本までやった

--- Chapter2/MyPlayground.playground/Contents.swift
+++ Chapter2/MyPlayground.playground/Contents.swift
@@ -61,7 +61,102 @@ array1.append("ばなな")
 //不変配列
 let array2: [String] = ["りんご","ごりら","らっぱ"]
 
+let first = array2[1]
+
+//辞書
+var dic1 :[String:String] = [String:String]();
+dic1["ゴリラ"] = "１体"
+dic1["りんご"] = "１個"
+
+let dic2 : [String:String] = ["ばなな":"1本"]
+
+//Optionalとnil
+var strOp :String?
+strOp = "こんにちは"
+if let strOp = strOp {
+    print(strOp)
+}
 
 
+//戻り値つき関数
+func myFunc1(funtInt :Double) -> Double {
+    let result: Double = funtInt * 1.08
+    return result
+}
+
+print("遊戯王パックの税込みの値段は",Int(myFunc1(funtInt: 150)),"円です")
+
+//引数なし関数
+func myFunc2(){
+    print("myFunc2!")
+}
+
+myFunc2()
+
+//引数がある関数
+//func myFunc3(num1 :Int ,num2 :Int){
+//    let num3:Int = num1 + num2
+//    print(num3)
+//}
+//
+//myFunc3(num1: 1010, num2: 2020)
+
+//エラーthrow処理
+//enum MyError: Errot{
+//    case InvalidValue
+//}
+//
+//func doubleUp(value; Int) throw -> Int {
+//    if value < 0{
+//        throw MyError.InvalidValue
+//    }
+//    return value * 2
+//}
+
+//上記に加えてエラーハンドリングも必要
+
+//TypeAlias
+typealias ColorCode = UInt8
+let r = ColorCode.max
+
+//クラスからインスタンス化
+var radio = UISwitch()
+
+//メソッドのプロパティ操作
+radio.isOn = true
+
+//メソッドのプロパティ変更
+radio.setOn(false, animated: true)
+
+class Dog{
+    var name = ""
+    var type = ""
+    var birthday = ""
+    
+    func bark(){
+        print("わんわん")
+    }
+}
+
+class GuideDog: Dog{
+    override func bark() {
+        print("ぐわんぐわん")
+    }
+}
+
+var dog = Dog()
+dog.bark()
+var GDog = GuideDog()
+GDog.bark()
+
+//タプル
+//複数の値をまとめることができる
+func requestMinAndMax() -> (min:Int, max:Int){
+    return (1,100)
+}
+
+let minAndMax = requestMinAndMax()
+let min = requestMinAndMax().min
+let max = requestMinAndMax().max
 
 
