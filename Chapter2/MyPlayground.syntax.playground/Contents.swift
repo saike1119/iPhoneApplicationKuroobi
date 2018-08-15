d6f920160839f4fae9fe2ba3731801b0499d4a80
saike1119 <saike1119@gmail.com>
2018-08-15 20:11:57 +0900

    構文追加

--- Chapter2/MyPlayground.syntax.playground/Contents.swift
+++ Chapter2/MyPlayground.syntax.playground/Contents.swift
@@ -29,15 +29,15 @@ if valueNumIf < 10 {
 }
 
 //switch
-//let valueSwitch: String = "みかん"
-//switch valueSwitch {
-//case "りんご":
-//    print("yes!りんご")
-//case "みかん":
-//    print("yes!みかん")
-//default:
-//    print("どちらでもない")
-//}
+let valueSwitch: String = "みかん"
+switch valueSwitch {
+case "りんご":
+    print("yes!りんご")
+case "みかん":
+    print("yes!みかん")
+default:
+    print("どちらでもない")
+}
 
 
 //while
@@ -49,5 +49,40 @@ while count <= 10 {
 }
 
 print("合計は\(result)")
+
+//gurad
+func buyItem(money :Int?){
+    guard let money = money else {
+        return
+    }
+    print("\(money)円分購入しました")
+}
+
+buyItem(money: nil)
+
+//filter
+//配列の中からある値を比較し出力する
+var values = [2,4,5,6,7,8,9,9,11,22,33]
+//let res = values.filter($0 > 10)
+
+//map
+//配列の中の値を全て２倍にする
+
+//reduce
+//配列の中の値を全て足す
+
+//sort
+//配列の中を昇順か降順に整える
+
+//for-in-where(フィルタリング)
+for value in 0..<9 where value != 5 {
+    print("\(value)")
+}
+
+//case-let-where
+//caseで条件をつけることができる
+
+//for-case-in
+//enumをforにかけるとき値を指定できる文
 
 
