1524ed17b66086acd9916c951884328cdcdf9e52
saike1119 <saike1119@gmail.com>
2018-09-07 22:16:02 +0900

    割引画面作成

--- Chapter4/MyCalc/MyCalc/PercentViewController.swift
+++ Chapter4/MyCalc/MyCalc/PercentViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class PercentViewController: UIViewController {
+    
+    // 金額を受け取るプロパティ
+    var price: Int = 0
+    
+    // 割引パーセンテージ入力フィールド
+    @IBOutlet weak var precentField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +27,91 @@ class PercentViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func tap1Button(_ sender: Any) {
+        let value = precentField.text! + "1"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap2Button(_ sender: Any) {
+        let value = precentField.text! + "2"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap3Button(_ sender: Any) {
+        let value = precentField.text! + "3"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap4Button(_ sender: Any) {
+        let value = precentField.text! + "4"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap5Button(_ sender: Any) {
+        let value = precentField.text! + "5"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap6Button(_ sender: Any) {
+        let value = precentField.text! + "6"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap7Button(_ sender: Any) {
+        let value = precentField.text! + "7"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap8Button(_ sender: Any) {
+        let value = precentField.text! + "8"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap9Button(_ sender: Any) {
+        let value = precentField.text! + "8"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap0Button(_ sender: Any) {
+        let value = precentField.text! + "0"
+        if let price = Int(value){
+            
+            precentField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tapClearButton(_ sender: Any) {
+        precentField.text = "0"
+    }
+    
+    
     /*
     // MARK: - Navigation
 
