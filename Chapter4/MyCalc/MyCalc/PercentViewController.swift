72501013a73d3f4f13476cc707834206ab051481
saike1119 <saike1119@gmail.com>
2018-09-07 22:00:50 +0900

    パーセントの画面とswift作成

---
+++ Chapter4/MyCalc/MyCalc/PercentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PercentViewController.swift
+//  MyCalc
+//
+//  Created by saizoukun on 2018/09/07.
+//  Copyright © 2018年 齋圭作. All rights reserved.
+//
+
+import UIKit
+
+class PercentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
